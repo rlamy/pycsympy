3f458fdaa90db1210e6a2b2f872acdd56d4e07f8
Ronan Lamy <Ronan.Lamy@normalesup.org>
2013-10-03 18:38:59 +0100

    implement __add__

--- pycsympy/basic.py
+++ pycsympy/basic.py
@@ -2,9 +2,13 @@ class Basic(object):
     def as_term(self):
         return Add(0, {self: 1})
 
+    def __add__(self, other):
+        return add(self, other)
+    __radd__ = __add__
+
 
 class Symbol(Basic):
     def __init__(self, name):
         self.name = name
 
-from pycsympy.add import Add
+from pycsympy.add import Add, add
--- pycsympy/test_add.py
+++ pycsympy/test_add.py
@@ -1,9 +1,9 @@
 from pycsympy.basic import Symbol
-from pycsympy.add import Add, add
+from pycsympy.add import Add
 
 def test_add_basic():
     x, y, z = map(Symbol, 'xyz')
-    assert isinstance(add(x, y), Add)
-    assert isinstance(add(x, 0), Symbol)
-    assert add(add(x, y), z) == add(x, add(y, z))
-    assert add(x, y) == add(y, x)
+    assert isinstance(x + y, Add)
+    assert isinstance(x + 0, Symbol)
+    assert (x + y) + z == x + (y + z)
+    assert x + y == y + x
